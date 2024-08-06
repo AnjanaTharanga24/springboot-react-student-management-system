e4a13f6a70b97518e5b62d50b0319ec4a7edcba7
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2024-08-06 17:40:16 +0530

    new update on the footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -132,17 +132,7 @@ export default function Footer() {
       </div>
     </section>
     {/* Section: Links  */}
-    {/* Copyright */}
-    <div
-      className="text-center p-4"
-      style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
-    >
-      © 2021 Copyright:
-      <a className="text-reset fw-bold" href="https://mdbootstrap.com/">
-        MDBootstrap.com
-      </a>
-    </div>
-    {/* Copyright */}
+   
   </footer>
   {/* Footer */}
 </>
