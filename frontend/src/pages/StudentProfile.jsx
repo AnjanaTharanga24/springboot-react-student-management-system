1655e81487d044af8a6b5c06ef1cefd2734a313a
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2024-08-12 00:22:50 +0530

    add confirmation popup alert for the delete function

--- frontend/src/pages/StudentProfile.jsx
+++ frontend/src/pages/StudentProfile.jsx
@@ -8,37 +8,44 @@ import axios from "axios";
 import Swal from "sweetalert2";
 import { Link, useNavigate } from "react-router-dom";
 export default function StudentProfile() {
-    const { user, setUser } = useContext(UserContext);
-    const navigate = useNavigate();
-    
-    const handleDelete = async () => {
-        try {
-          const response = await axios.delete(`http://localhost:8082/students/${user.id}`);
+  const { user, setUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleDelete = async () => {
+    try {
+      const response = await axios.delete(
+        `http://localhost:8082/students/${user.id}`
+      );
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      }).then((result) => {
+        if (result.isConfirmed) {
           setUser(null);
-          Swal.fire({
-            position: "center",
-            icon: "success",
-            title: "Account deleted successfully!",
-            showConfirmButton: false,
-            timer: 1500
-          });
-          navigate('/');
-        } catch (error) {
-          console.error("Error deleting account: ", error.response || error);
-          let errorMessage = "Failed to delete account";
-          if (error.response) {
-            errorMessage += `: ${error.response.status} ${error.response.statusText}`;
-          }
-          Swal.fire({
-            position: "center",
-            icon: "error",
-            title: errorMessage,
-            showConfirmButton: false,
-            timer: 1500
-          });
+          navigate("/");
         }
-      };
-    
+      });
+    } catch (error) {
+      console.error("Error deleting account: ", error.response || error);
+      let errorMessage = "Failed to delete account";
+      if (error.response) {
+        errorMessage += `: ${error.response.status} ${error.response.statusText}`;
+      }
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: errorMessage,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  };
+
   return (
     <div className="student-profile">
       <div>
@@ -50,67 +57,72 @@ export default function StudentProfile() {
           <img src={profileImage} className="profile-image" />
           <p className="profile-name">{user.name}</p>
           <div className="p-4">
-            <Link to='/update-student-profile'>
-            <button className="btn btn-primary edit-btn ">Edit profile</button>
+            <Link to="/update-student-profile">
+              <button className="btn btn-primary edit-btn ">
+                Edit profile
+              </button>
             </Link>
-            <button className="btn btn-danger delete-btn" onClick={handleDelete}>
+            <button
+              className="btn btn-danger delete-btn"
+              onClick={handleDelete}
+            >
               Delete account
             </button>
           </div>
         </div>
         <div className="d-flex profile-details-card  text-start">
-            <div className="main-text fs-2">
-                <div className="p-1">
-                    <label>Name</label>
-                </div>
-                <div className="p-1">
-                    <label>Username</label>
-                </div>
-                <div className="p-1">
-                    <label>Age</label>
-                </div>
-                <div className="p-1">
-                    <label>Gender</label>
-                </div>
-                <div className="p-1">
-                    <label>Address</label>
-                </div>
-                <div className="p-1">
-                    <label>Mobile</label>
-                </div>
-                <div className="p-1">
-                    <label>Email</label>
-                </div>
-                <div className="p-1">
-                    <label>Date of birth</label>
-                </div>
+          <div className="main-text fs-2">
+            <div className="p-1">
+              <label>Name</label>
             </div>
-            <div className="value-text fs-2">
-                <div className="p-1">
-                    <label >{user.name}</label>
-                </div>
-                <div className="p-1">
-                    <label>{user.username}</label>
-                </div>
-                <div className="p-1">
-                    <label>{user.age}</label>
-                </div>
-                <div className="p-1">
-                    <label>{user.gender}</label>
-                </div>
-                <div className="p-1">
-                    <label>{user.address}</label>
-                </div>
-                <div className="p-1">
-                    <label>{user.mobile}</label>
-                </div>
-                <div className="p-1">
-                    <label>{user.email}</label>
-                </div>
-                <div className="p-1">
-                    <label>{user.dob}</label>
-                </div>
+            <div className="p-1">
+              <label>Username</label>
             </div>
+            <div className="p-1">
+              <label>Age</label>
+            </div>
+            <div className="p-1">
+              <label>Gender</label>
+            </div>
+            <div className="p-1">
+              <label>Address</label>
+            </div>
+            <div className="p-1">
+              <label>Mobile</label>
+            </div>
+            <div className="p-1">
+              <label>Email</label>
+            </div>
+            <div className="p-1">
+              <label>Date of birth</label>
+            </div>
+          </div>
+          <div className="value-text fs-2">
+            <div className="p-1">
+              <label>{user.name}</label>
+            </div>
+            <div className="p-1">
+              <label>{user.username}</label>
+            </div>
+            <div className="p-1">
+              <label>{user.age}</label>
+            </div>
+            <div className="p-1">
+              <label>{user.gender}</label>
+            </div>
+            <div className="p-1">
+              <label>{user.address}</label>
+            </div>
+            <div className="p-1">
+              <label>{user.mobile}</label>
+            </div>
+            <div className="p-1">
+              <label>{user.email}</label>
+            </div>
+            <div className="p-1">
+              <label>{user.dob}</label>
+            </div>
+          </div>
         </div>
       </div>
 
