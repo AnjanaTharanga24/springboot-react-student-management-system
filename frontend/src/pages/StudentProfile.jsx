fe958748acc7acd2740ec0b6d36bc8231a24649c
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2024-08-12 00:42:30 +0530

    change swal alert on the register function

--- frontend/src/pages/StudentProfile.jsx
+++ frontend/src/pages/StudentProfile.jsx
@@ -18,7 +18,6 @@ export default function StudentProfile() {
       );
       Swal.fire({
         title: "Are you sure?",
-        text: "You won't be able to revert this!",
         icon: "warning",
         showCancelButton: true,
         confirmButtonColor: "#3085d6",
